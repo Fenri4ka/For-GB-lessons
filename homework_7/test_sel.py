d1454ae56cd09047429b5239bac6b811265d6158
Fenri4ka <98591203+Fenri4ka@users.noreply.github.com>
2024-08-08 18:59:38 +0300

    Delete homework_7/test_sel.py
    del

--- homework_7/test_sel.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-df = pd.read_json('quotes.csv')
-#типы данных
-print(df.dtypes)
-#20 самых залайканных цитат
-print(df.sort_values('likes', ascending=False).head(20))
-#сортировка по самым богатым на цитаты авторам
-print(df['author'].value_counts())
